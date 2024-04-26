6fea013aafa01082f8d8064b453a059164657efe
mijakub <kuba.mura@wp.pl>
2024-04-26 18:52:55 +0200

    Punkty i szybkość

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 const field = document.getElementById("playingField");
 const startCard = document.getElementById("start");
+const scoreText = document.getElementById("score");
+const highScoreText = document.getElementById("highscore");
 let snake = [{x: 10, y: 10}];
 let food = {x: random(), y: random()};
 let direction = "left";
-let gameSpeed = 100;
+let gameSpeed = 180;
+let score = 0;
+let highScore = 0;
 
 function random(){
     return Math.floor(Math.random() * 20) + 1;
@@ -60,6 +64,9 @@ const moveSnake = () => {
     }
     snake.unshift(head);
     if(head.x == food.x && head.y == food.y){
+        updateSpeed();
+        score++;
+        scoreText.innerHTML = `Score: ${score}`;
         removeAll(field, "food");
         food = {x: random(), y: random()}
         makeFood();
@@ -88,9 +95,14 @@ const stopGame = () => {
     removeAll(field, "snake");
     removeAll(field, "food");
     clearInterval(game);
+    updateHS();
     snake = [{ x: 10, y: 10 }];
     direction = "left";
     food = {x: random(), y: random()};
+    score = 0;
+    gameSpeed = 180;
+    scoreText.innerHTML = `Score: ${score}`;
+    highScoreText.innerHTML = `Highest Score: ${highScore}`;
 }
 
 const collision = () => {
@@ -107,6 +119,18 @@ const collision = () => {
     })
 }
 
+const updateHS = () => {
+    if(score > highScore){
+        highScore = score;
+    }
+}
+
+const updateSpeed = () => {
+    if (gameSpeed > 150) gameSpeed -= 5;
+    else if (gameSpeed > 120) gameSpeed -= 3;
+    else if (gameSpeed > 80) gameSpeed -= 2;
+}
+
 document.addEventListener("keydown", (event) => {
     switch(event.key){
         case " ":
@@ -114,38 +138,30 @@ document.addEventListener("keydown", (event) => {
             break;
         case "ArrowUp":
             direction = "up";
-            console.log(direction);
             break;
         case "ArrowDown":
             direction = "down";
-            console.log(direction);
             break;
         case "ArrowLeft":
             direction = "left";
-            console.log(direction);
             break;
         case "ArrowRight":
             direction = "right";
-            console.log(direction);
             break;
         case "w":
             direction = "up";
-            console.log(direction);
             drawSnake()
             break;
         case "s":
             direction = "down";
-            console.log(direction);
             drawSnake()
             break;
         case "a":
             direction = "left";
-            console.log(direction);
             drawSnake()
             break;
         case "d":
             direction = "right";
-            console.log(direction);
             drawSnake()
             break;
     }
